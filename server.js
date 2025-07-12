52e6df414540d489b38305ef198b4d028098f75c
Lucas LaVilla <lucas.lavilla@mail.com>
2025-07-12 16:15:26 -0400

    organized file structure and made 'users' table store info from signup page

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const pool = require('./db');
+const pool = require('./js/db');
 const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
 app.use(express.static(__dirname));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
 
 //make 'localhost:8080/signup' path connect to signup page
 app.get('/signup', (req,res) => {
@@ -22,7 +26,7 @@ app.get('/login', (req, res) => {
 app.post('/signup', (req,res) => {
   const { username, email, password} = req.body;
   const sql = `INSERT INTO users (username,email,password)
-               VALUES($1, $2, $3) RETURNING id`;
+               VALUES($1, $2, $3) RETURNING user_id`;
   pool.query(sql, [username, email, password], (err, result) => {
     if(err){
       console.log(err);
@@ -48,6 +52,7 @@ app.post('/login', (req, res) => {
     }
   )
 })
+
 /*
 //-----Database connection-----
 app.get('/', (req, res) => {
@@ -56,7 +61,7 @@ app.get('/', (req, res) => {
       console.error(err);
       res.status(500).send('Database connection failed');
     } else {
-      res.send(`Connected! Time: ${result.rows[0].now}`);
+      res.send(`Connected`);
     }
   });
 });
